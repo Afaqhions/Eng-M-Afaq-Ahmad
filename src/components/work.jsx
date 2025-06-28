65361f7c29b73982dc8bf7001bd32e30965e1588
Afaqhions <m.afaqpak@gmail.com>
2025-06-28 22:22:09 +0500

    Update work.jsx

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -5,13 +5,13 @@ const Work = () => {
     <div>
       <section class="py-16 px-4 sm:px-6 lg:px-20">
         <div class="max-w-7xl mx-auto">
-          <h2 class="text-3xl font-bold text-center dark:text-red-500 mb-12">
+          <h2 class="text-3xl font-bold text-center text-[#8245ec] mb-12">
             Projects
           </h2>
 
           <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {/* <!-- Project 1: React --> */}
-            <div class="bg-red-500 rounded-lg shadow hover:shadow-xl transition duration-300 p-6">
+            <div class="border-2 border-[#8245ec] rounded-lg shadow hover:shadow-xl transition duration-300 p-6">
               <h3 class="text-xl font-semibold text-gray-800 dark:text-white mb-2">
                 Portfolio Website
               </h3>
@@ -25,13 +25,14 @@ const Work = () => {
             </div>
 
             {/* <!-- Project 2: Django --> */}
-            <div class="bg-red-500 rounded-lg shadow hover:shadow-xl transition duration-300 p-6">
+            <div class="border-2 border-[#8245ec] rounded-lg shadow hover:shadow-xl transition duration-300 p-6">
               <h3 class="text-xl font-semibold text-gray-800 dark:text-white mb-2">
                 SARA-AI
               </h3>
               <p class="text-sm text-[#fff] mb-4">
-                A full-stack chatbot application having AI's for Study, Coding, Health and for general use using Django and Sqlite3
-                with authentication and admin panel.
+                A full-stack chatbot application having AI's for Study, Coding,
+                Health and for general use using Django and Sqlite3 with
+                authentication and admin panel.
               </p>
               <span class="inline-block bg-green-100 dark:bg-green-800 text-green-800 dark:text-white text-[18px] font-medium px-2 py-1 rounded">
                 Django
@@ -39,13 +40,14 @@ const Work = () => {
             </div>
 
             {/* <!-- Project 3: ML --> */}
-            <div class="bg-red-500 rounded-lg shadow hover:shadow-xl transition duration-300 p-6">
+            <div class="border-2 border-[#8245ec] rounded-lg shadow hover:shadow-xl transition duration-300 p-6">
               <h3 class="text-xl font-semibold text-gray-800 dark:text-white mb-2">
                 Spam Email & Message Checker
               </h3>
               <p class="text-sm 0 text-[#fff] mb-4">
                 A machine learning project using Scikit-learn and Python to
-                predict whether a message or an email is Spam or not from dataset features.
+                predict whether a message or an email is Spam or not from
+                dataset features.
               </p>
               <span class="inline-block bg-yellow-100 dark:bg-yellow-600 text-yellow-800 dark:text-white text-[18px] font-medium px-2 py-1 rounded">
                 ML / Python
